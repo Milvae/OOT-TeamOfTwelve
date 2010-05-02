b13c310dd1dc6ec9bb9ca146f632083ada6ba91f
Kristian Kraljic <kriskra@gmail.com>
2010-05-02 20:28:37 +0200

    second commit

--- src/HelloTeamOfTwelve.java
+++ src/HelloTeamOfTwelve.java
@@ -4,6 +4,7 @@
  */
 public class HelloTeamOfTwelve {
 	public static void main(String args[]) {
+		//Don't forget about some inline comments for hard to understand code
 		System.out.print("Hello TeamOfTwelve!");
 	}
 }
